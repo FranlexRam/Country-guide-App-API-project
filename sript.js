bccdc884f307d0b6da7720c15492bf606b40ccdf
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-01-12 17:48:09 -0400

    Fixing code

--- sript.js
+++ sript.js
@@ -11,13 +11,14 @@ https://restcountries.com/v3.1/name/{name}
 // SELECTORES
 let searchInput = document.getElementById('buscador');
 // EVENT LISTENER
-searchInput.addEventListener("click", async (e)=>{
+searchInput.addEventListener("click", ()=>{
     let countryName = searchInput.value;
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}`;
+    console.log(finalURL);
     fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        console.log(data[0]);
         // console.log(data[0].flag);
         // console.log(data[0].name);
         // console.log(data[0].capital);
@@ -27,25 +28,25 @@ searchInput.addEventListener("click", async (e)=>{
         // let countriesArray = data.slice(0,9);
         // countriesArray.forEach(country => {
         
-        result.innerHTML += `
-        <img src="${data.flag}" class="flag-img">
-        <h2>${data.name}</h2>
+        result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
         <div class="wrapper">
             <div class="data-wrapper">
                 <h4>Capital:</h4>
-                <span>${data.capital}</span>
+                <span>${data[0].capital}</span>
             </div>
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
                 <h4>Population:</h4>
-                <span>${data.population}</span>
+                <span>${data[0].population}</span>
             </div>
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
                 <h4>Region:</h4>
-                <span>${data.region}</span>
+                <span>${data[0].region}</span>
             </div>
         </div>
         `;
