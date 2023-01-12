a4f47805238adecfdeeb0e8a405540f0877056ff
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-01-11 22:31:38 -0400

    Using API

--- sript.js
+++ sript.js
@@ -1,16 +1,64 @@
-// API's
-const API_PAISES = 'https://restcountries.com/';
-const API_CLIMA = 'https://openweathermap.org/current';
+// Links API's
+/*
+                //FULL NAME
+https://restcountries.com/v3.1/name/{name}?fullText=true
+                //NAME
+https://restcountries.com/v3.1/name/{name}
+*/
+
 
 
 // SELECTORES
-const buscador = document.querySelector("#buscador");
-
-fetch(`${API_PAISES}/users`)
+let searchInput = document.getElementById('buscador');
+// EVENT LISTENER
+searchInput.addEventListener("click", async (e)=>{
+    let countryName = searchInput.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    fetch(finalURL)
     .then((response) => response.json())
-    .then((users) =>{
-        const tpl = users.map(user => `<li></li>`)
-    });
+    .then((data) => {
+        console.log(data);
+        // console.log(data[0].flag);
+        // console.log(data[0].name);
+        // console.log(data[0].capital);
+        // console.log(data[0].population);
+        // console.log(data[0].region);
+        
+        // let countriesArray = data.slice(0,9);
+        // countriesArray.forEach(country => {
+        
+        result.innerHTML += `
+        <img src="${data.flag}" class="flag-img">
+        <h2>${data.name}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data.capital}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data.population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Region:</h4>
+                <span>${data.region}</span>
+            </div>
+        </div>
+        `;
 
-    const data = JSON.parse(this.response);
-    console.log(data);
+    })
+    .catch(() => {
+        if (searchInput.length == 0) {
+            result.innerHTML = `<h3>The input field cannot be empty</h3>`
+        } else {
+            result.innerHTML = `<h3>Please enter a valid country name</h3>`
+        }
+    })
+
+
+    // });
+});
