b48f6f040da51b2bda8b3fbe67da7df2b8a4c10c
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-01-12 19:19:54 -0400

    IMPLEMENTING CODE

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+// const result = document.getElementById('result')
+
+document.addEventListener("DOMContentLoaded", e => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('testApi.json')
+        const data = await res.json()
+        banderillas(data)
+        formularioCliente(data)
+        filtros(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const banderillas = data => {
+    let elementos = ''
+    data.forEach(item => {
+        elementos += `
+        <img src="${item.flags}" class="flag-img">
+        <h2>${item.name}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${item.capital}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${item.population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Region:</h4>
+                <span>${item.region}</span>
+            </div>
+        </div>
+        `
+    });
+    result.innerHTML = elementos
+}
+
+
+
+// INPUT
+let searchInput = document.getElementById('buscador');
+
+const formularioCliente = data => {
+    searchInput.addEventListener('keyup', e => {
+        e.preventDefault()
+        const letraCliente = searchInput.value.toLowerCase()
+        // console.log(letraCliente)
+        const arrayFiltrado = data.filter(item => {
+            const letraApi = item.name.toLowerCase()
+            if (letraApi.indexOf(letraCliente) !== -1) {
+                return item
+            }
+        })
+        banderillas(arrayFiltrado)
+    })
+}
